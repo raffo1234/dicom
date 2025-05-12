0f7a9581b9bc92b792baf14dc9987400b7cb02fd
Rafael <rhmcord@gmail.com>
2025-05-12 10:27:29 -0500

    feat: add link to all files

--- src/app/(admin)/admin/dicom/page.tsx
+++ src/app/(admin)/admin/dicom/page.tsx
@@ -5,6 +5,8 @@ import { auth } from "@/lib/auth";
 import { supabase } from "@/lib/supabase";
 // import DicomMetadataUploadLargeFile from "@/components/DicomMetadataUploadLargeFile";
 import { Permissions } from "@/types/propertyState";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await auth();
@@ -22,12 +24,22 @@ export default async function Page() {
 
   return (
     <>
-      <h1 className="mb-6 font-semibold text-lg block">
-        Upload Files{" "}
-        <span className="text-sm text-gray-500 font-normal">
-          Compressed files containing (.dcm) files
-        </span>
-      </h1>
+      <div className="flex justify-between">
+        <h1 className="mb-6 font-semibold text-lg block">
+          Upload Files{" "}
+          <span className="text-sm text-gray-500 font-normal">
+            Compressed files containing (.dcm) files
+          </span>
+        </h1>
+        <Link
+          href="/admin/dicoms"
+          className="flex items-center gap-2 cursor-pointer text-center p-3 text-cyan-400 group"
+          title="Add template"
+        >
+          <Icon icon="solar:file-text-line-duotone" fontSize={24} />
+          <span className="group-hover:underline">View All</span>
+        </Link>
+      </div>
       <CheckPermission
         userRoleId={user.role_id}
         requiredPermission={Permissions.CARGAR_DICOM}
